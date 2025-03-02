53ca311858de5a8b31bf7e79af9e92c2d58dc88a
rebecca.magi@myy.haaga-helia.fi <rebecca.magi@myy.haaga-helia.fi>
2025-03-02 14:35:44 +0200

    parse

--- oma.js
+++ oma.js
@@ -19,6 +19,10 @@ function kerro(data) {
     teksti = teksti + "<p>" + "<b>" + "Tekniikat" + "</b>" + "<br>"
         + data.tekniikat + "</p>";
 
+        const json = '{"result":true, "count":42}';
+        const obj = JSON.parse(json);    
+        const teksti = obj.count + ' & ' + obj.result;    
+
     document.getElementById("vastaus").innerHTML = teksti;
 }
 
