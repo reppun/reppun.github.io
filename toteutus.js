eaaee1c1bcef86437692b7b1593cad36bc790cba
rebecca.magi@myy.haaga-helia.fi <rebecca.magi@myy.haaga-helia.fi>
2025-03-02 15:25:18 +0200

    toteutusta

---
+++ toteutus.js
@@ -0,0 +1,35 @@
+function kerro(data) {
+    var teksti = "";
+
+    teksti = "<h2>" + data.nimi + "</h2>";
+
+    teksti = teksti + "<p>" + "<b>" + "Osallistujat" + "</b>" + "<br>"
+        + data.osallistujat + "<br>";
+
+    teksti = teksti + "<p>" + "<b>" + "Toteutuksen päivämäärät" + "</b>" + "<br>"
+        + data.pvm.alku + "-" + data.pv.loppu + "</p>";
+
+    teksti = teksti + "<p>" + "<b>" + "Kesto" + "</b>" + "<br>"
+        + data.kesto + "</p>";
+
+    document.getElementById("vastaus").innerHTML = teksti;
+
+}
+
+fetch('https://reppun.github.io/toteutus.json')
+
+    // Muunnetaan vastaus JSON muotoon  
+    .then(function (response) {
+        return response.json();
+    })
+
+    //Käsitellään muunnettu (eli JSON muotoinen) vastaus
+    // Kutsutaan funktiota kerro()ja välitetään sille json-vastaus
+    .then(function (responseJson) {
+        kerro(responseJson);
+    })
+
+    // Jos tuli jokin virhe
+    .catch(function (error) {
+        document.getElementById("vastaus").innerHTML = "<p>Tietoa ei pystytä hakemaan, mutta miksi? Hmm...</p>";
+    })
